1c87eb61bd2118024ae8fb36bb6ec488e9ab9f87
Ian Irving <ian@falsepositives.com>
2017-07-14 12:09:34 -0400

    Initial commit

---
+++ src/ui/components/track-weather/component.ts
@@ -0,0 +1,36 @@
+import Component, { tracked } from "@glimmer/component";
+
+export default class TrackWeather extends Component {
+	@tracked weather;
+	@tracked area;
+	@tracked addZip;
+
+	constructor(options) {
+		super(options);
+		this.loadWeather();
+	}
+
+ onKeyUp(event) {
+    this.addZip = event.target.value;
+  }
+  clicked () {
+  		this.loadWeather();
+  }
+
+	async loadWeather() {
+		let zip = this.addZip || 97239;
+		console.log(zip);
+
+		let response1 = await fetch(`http://api.wunderground.com/api/625172310aff38a6/geolookup/q/${zip}.json`);
+		this.area = await response1.json();
+
+		let city = this.area.location.city;
+		let state = this.area.location.state;
+
+		let response2 = await fetch(`http://api.wunderground.com/api/625172310aff38a6/conditions/q/${state}/${city}.json`);
+		this.weather = await response2.json();
+		console.log(this.weather.current_observation.temp_f);
+				console.log(this.weather.current_observation.temp_c);
+		setTimeout( () => { this.loadWeather(); }, 10000);
+	}
+};
